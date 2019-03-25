bb9172e5106a6a6b01a586ff3df4966f10c1d771
tpreilly4 <tpreilly4@gmail.com>
2019-03-25 17:45:27 -0400

    POST Route is setup. Hibernate permissions set to none

---
+++ sql-scripts/init.sql
@@ -0,0 +1,2 @@
+INSERT INTO beer ( id, name, type, brewery, description, abv ) VALUES
+  ('1', 'Boston Lager','Vienna Lager','Sam Adams','Full-flavored with a balance of malty sweetness contrasted by hop spiciness and a smooth finish.','5.00');
